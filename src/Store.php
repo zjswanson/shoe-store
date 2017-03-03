87b0206ac776364644b7e0808cc582ff8e1729f7
Zach Swanson <zach.j.swanson@gmail.com>
2017-03-03 09:06:35 -0800

    add and test get and set methods for Store class

--- src/Store.php
+++ src/Store.php
@@ -13,6 +13,29 @@
             $this->id = $id;
         }
 
-        
+        function getName()
+        {
+            return $this->name;
+        }
+        function setName($new_name)
+        {
+            $this->name = $new_name;
+        }
+
+        function getTargetMarket()
+        {
+            return $this->target_market;
+        }
+        function setTargetMarket($new_target_market)
+        {
+            $this->target_market = $new_target_market;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+
     }
 ?>
--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -23,7 +23,7 @@
             $result = array($test_store->getName(), $test_store->getTargetMarket(), $test_store->getId());
             $expected_result = array($name,$target_market,$id);
             // Assert
-            $this->assertEquals($result, $expected_result);
+            $this->assertEquals($expected_result, $result);
         }
 
         function test_setters()
@@ -33,16 +33,18 @@
             $name = 'ShoEmporium';
             $target_market = "discount";
             $test_store = new Store ($name,$target_market,$id);
-            $id = 1;
+
             $name2 = 'Fancy Feet';
             $target_market2 = "luxury";
             // Act
             $test_store->setName($name2);
-            $test_store->setName($target_market2);
+            $test_store->setTargetMarket($target_market2);
+
             $result = array($test_store->getName(), $test_store->getTargetMarket(), $test_store->getId());
             $expected_result = array($name2,$target_market2,$id);
+
             // Assert
-            $this->assertEquals($result, $expected_result);
+            $this->assertEquals($expected_result, $result);
         }
     }
 ?>
